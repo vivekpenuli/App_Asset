47106a3343468af248a82a21503dae22d219c5ba
vivek penuli <63710710+vivekpenuli@users.noreply.github.com>
2023-10-05 17:11:32 +0530

    Update Recyvlerview.kt

--- Recyvlerview.kt
+++ Recyvlerview.kt
@@ -8,7 +8,7 @@ import com.example.food_admin.databinding.CartitemBinding
 // Replace with your actual package name  
 // Note: The file name should be the same as of recylecer view calss name
 
-class CartBindingAdapter(private val dataSet: List<cartdata>) :
+class CartBindingAdapter(private val dataSet: List<cartdata>) :           // List<cardata> : cardata is the type of data which my list will have
     RecyclerView.Adapter<CartBindingAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
