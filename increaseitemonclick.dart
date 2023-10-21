6e04ff9b9b02bef25fe292b37fb77d80928fa10b
vivek penuli <63710710+vivekpenuli@users.noreply.github.com>
2023-10-21 19:58:18 +0530

    Update increaseitemonclick.dart

--- increaseitemonclick.dart
+++ increaseitemonclick.dart
@@ -4,7 +4,12 @@
 
                 val currentPrice = item.foodPrice?.toIntOrNull() ?: 0
                 item.foodPrice = (currentPrice + (item.baseprice?.toInt() ?: 0)).toString()
-                  // baseprice help to retian the actutal price even when the UI update
+                  /* baseprice help to retian the actutal price even when the UI update .
+                   Becuse what happen is if i do like this,
+                    val currentPrice = item.foodPrice?.toIntOrNull() ?: 0
+                 item.foodPrice = (currentPrice + (item.foodprice?.toInt() ?: 0)).toString()
+                 here we do item.foodprice added to our current price. But what happen is when UI updated itemprice also updated
+                  */
                 println("base price ${item.baseprice?.toInt() ?: 0}")
                 binding.dishCostTextView.text = item.foodPrice
 
