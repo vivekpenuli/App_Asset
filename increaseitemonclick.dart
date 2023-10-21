4fd82aa0153f1cbbb9b2eca42d1608e75e4b09f0
vivek penuli <63710710+vivekpenuli@users.noreply.github.com>
2023-10-21 19:13:59 +0530

    Update increaseitemonclick.dart

--- increaseitemonclick.dart
+++ increaseitemonclick.dart
@@ -13,3 +13,12 @@
 
 
 //////////////////////////////////
+data class AddtoFirebase(val foodName :String?= null,
+                         var foodPrice:String?=null,
+                         val foodDisc :String?=null,
+                         val foodImg :String?=null,
+                         val foodIngred :String?= null,
+                         var fooddQuantity:Int ?=null,
+    var baseprice:String?=null// act as base price for food item for adding that item
+
+)
