063eff692be4f16e892557865309d8be339aa395
vivek penuli <63710710+vivekpenuli@users.noreply.github.com>
2023-10-21 19:12:02 +0530

    Update increaseitemonclick.dart

--- increaseitemonclick.dart
+++ increaseitemonclick.dart
@@ -1,1 +1,11 @@
+  // Handle click on "Add" button
+            binding.addButton.setOnClickListener {
+                item.fooddQuantity = item.fooddQuantity!! + 1
 
+                val currentPrice = item.foodPrice?.toIntOrNull() ?: 0
+                item.foodPrice = (currentPrice + (item.baseprice?.toInt() ?: 0)).toString()
+                println("base price ${item.baseprice?.toInt() ?: 0}")
+                binding.dishCostTextView.text = item.foodPrice
+
+                binding.itemCountTextView.text = item.fooddQuantity.toString()
+            }
