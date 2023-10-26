6a2bc535f5fbbb05927f4b7f1b5c2f833aabd5cf
vivek penuli <63710710+vivekpenuli@users.noreply.github.com>
2023-10-26 15:06:55 +0530

    Data_Model

---
+++ DataModel.kt
@@ -0,0 +1,6 @@
+
+data class YourDataModel(val foodName :String?= null,
+                         val foodPrice:String?=null,
+                         val foodDisc :String?=null,
+                         val foodImg :String?=null,
+                         val foodIngred :String?= null)
