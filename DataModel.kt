1649f1403d54649efd92ec216652b887c3cffaa5
vivek penuli <63710710+vivekpenuli@users.noreply.github.com>
2023-10-26 15:08:12 +0530

    data_MODEL

--- DataModel.kt
+++ DataModel.kt
@@ -4,3 +4,12 @@ data class YourDataModel(val foodName :String?= null,
                          val foodDisc :String?=null,
                          val foodImg :String?=null,
                          val foodIngred :String?= null)
+
+/*
+Data Model Concept :
+In Firebase, a data model concept refers to how you structure and organize
+ your data within the Firebase Realtime Database or Firestore to efficiently store,
+  retrieve, and manage information for your Android app developed using Kotlin. 
+  The data model concept in Firebase is crucial for creating a well-organized and 
+  scalable database structure.
+*/
