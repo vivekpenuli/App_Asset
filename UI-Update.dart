ca6ca696379c79ddd0c990ebd3c621fcd169cfe9
vivek penuli <63710710+vivekpenuli@users.noreply.github.com>
2023-11-09 07:54:34 +0530

    Update UI-Update.dart

--- UI-Update.dart
+++ UI-Update.dart
@@ -23,3 +23,27 @@ Database and updates it in the app's UI. However, it appears that it's only upda
 To make it update in real-time as data changes in the database, you should use Firebase Realtime Database listeners. 
 Here's how you can modify your code to achieve real-time updates:
 */
+
+
+//Solution
+private fun pendingOrders() {
+    database = FirebaseDatabase.getInstance()
+    val pendingOrderReference = database.reference.child("OrderDetails")
+    val binding = // your binding reference
+
+    // Create a ValueEventListener to listen for changes
+    val valueEventListener = object : ValueEventListener {
+        override fun onDataChange(snapshot: DataSnapshot) {
+            val pendingOrderItemCount = snapshot.childrenCount.toInt()
+            binding.pendingOrdersCount.text = pendingOrderItemCount.toString()
+        }
+
+        override fun onCancelled(error: DatabaseError) {
+            // Handle the error here if needed
+        }
+    }
+
+    // Add the ValueEventListener to the reference
+    pendingOrderReference.addValueEventListener(valueEventListener)
+}
+// It ensure UI will update instanteaneiuly on any new change
