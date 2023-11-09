164c21d33044c21477d111edcab39abeb923b031
vivek penuli <63710710+vivekpenuli@users.noreply.github.com>
2023-11-09 07:53:19 +0530

    Update UI-Update.dart

--- UI-Update.dart
+++ UI-Update.dart
@@ -1,1 +1,19 @@
+// Proble UI Update
+private fun pendingOrders() {
+        database = FirebaseDatabase.getInstance()
+        var pendingOrderReference = database.reference.child("OrderDetails")
+        var pendingOrderItemCount = 0
+        pendingOrderReference.addListenerForSingleValueEvent(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
 
+pendingOrderItemCount =snapshot.childrenCount.toInt()
+                binding.pendingOrdersCount.text = pendingOrderItemCount.toString()
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
+    }
